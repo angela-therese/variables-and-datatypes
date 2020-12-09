9fa4b9355f7f3b84499882e3a2b2eb53ad3e4e4d
User-42 <a.t.iafrate@gmail.com>
2020-12-08 21:41:40 -0500

    javascript file to link

---
+++ script.js
@@ -0,0 +1,8 @@
+let aboutMe = {
+    firstName: 'Angie',
+    lastName: 'Iafrate',
+    age: 41,
+    likesTravel: true,
+    favoriteMovies: ["Fargo", "Il Postino", "Amadeus"]
+}
+console.log(aboutMe) 
