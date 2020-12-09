6e2dfafefb4ec57f1791f51195b089079549f8cf
User-42 <a.t.iafrate@gmail.com>
2020-12-08 22:12:13 -0500

    addition of hobbies

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let aboutMe = {
     lastName: 'Iafrate',
     age: 41,
     likesTravel: true,
-    favoriteMovies: ["Fargo", "Il Postino", "Amadeus"]
-    children: "Luca"
+    favoriteMovies: ["Fargo", "Il Postino", "Amadeus"],
+    children: "Luca",
+    favoriteHobbies: ['hula hoops', 'guitar', 'writing', 'coding']
 }
 console.log(aboutMe) 
